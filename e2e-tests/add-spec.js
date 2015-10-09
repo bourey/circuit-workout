d04db905ecdf2ae971c96f98afb7b28270f162bb
Jen Bourey <jennifer.bourey@gmail.com>
2015-10-09 14:37:45 -0700

    Adding sample integration test

---
+++ e2e-tests/add-spec.js
@@ -0,0 +1,13 @@
+describe('Add an exercise', function() {
+
+	beforeEach(function() {
+    browser.get('http://127.0.0.1/~bourey/circuit/app/#/add');
+	});
+
+	it('should display an error when the name is too long', function() {
+		element(by.model('ctrl.exercise.name')).
+				sendKeys('Very long exercise name that should be rejected');
+		browser.sleep(1000);
+		expect($('.errors').getText()).toContain('30 characters');
+	});
+});
